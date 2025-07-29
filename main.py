8d60a7f1de448dcceea3645a2e44cf07e3dc6571
perejilinformatico <perejilinformatico@gmail.com>
2025-07-29 01:52:58 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+caracteres = list("+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+Longitud = int(input("Escribe el numero de caracteres de tu contraseña: "))
+
+for i in range(Longitud):
+   Contraseña = random.choices(caracteres, k=Longitud)
+
+contraseña = ''.join(Contraseña)
+print(f"La contraseña generada es: {contraseña}")
